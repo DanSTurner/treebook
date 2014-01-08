ae8db694d4012448a248641c74f6a96c408d2a48
Dan S Turner <turner@nowwithmoredan.com>
2014-01-07 16:41:18 -0800

    Add devise users, views for same, figaro

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :profile_name, length: { minimum: 4 }, uniqueness: true
+end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Treebook::Application.config.secret_key_base = '1cec9a94036a8ad519cd61b882aa494253e695d91d87492da7744fab441c4ebb9739f139b087253629acdf220b40c6cdb097472419186937c69428bfd4aa2bb7'
+Treebook::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
